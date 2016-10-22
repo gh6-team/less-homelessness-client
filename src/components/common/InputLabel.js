6f3b8f2acc622b38400a99a77813a29519266745
Will Stampley <wstampley@gmail.com>
2016-10-22 17:47:24 -0500

    Common components and more intake survey stuff

---
+++ src/components/common/InputLabel.js
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react';
+import CommonStyling from "./CommonStyling";
+
+export default class InputLabel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillReceiveProps(nextProps) {
+  }
+  render() {
+    const style = {
+      height: CommonStyling.InputFieldHeight,
+      display: "table-cell",
+      padding: "1px 12px",
+      verticalAlign: "middle"
+    };
+    return (
+      <div style={style} >{this.props.value}</div>
+    );
+  }
+}
+
+InputLabel.propTypes = {
+  value: PropTypes.string
+};
+
+InputLabel.defaultProps = {
+};

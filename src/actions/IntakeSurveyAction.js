db611339f285ad789d8f984b6e042f4d915f4a36
Will Stampley <wstampley@gmail.com>
2016-10-23 02:22:25 -0500

    methods for saving the survey data

--- src/actions/IntakeSurveyAction.js
+++ src/actions/IntakeSurveyAction.js
@@ -1,26 +1,38 @@
-//import LHDispatcher from '../dispatchers/LHDispatcher';
-//import ActionTypes from '../constants/actionTypes';
-//import ClientProxy from "../proxys/ClientProxy";
+import LHDispatcher from '../dispatchers/LHDispatcher';
+import ActionTypes from '../constants/actionTypes';
+import ClientProxy from "../proxys/ClientProxy";
 
 export default class IntakeSurveyAction {
 
-  static saveIntakeSurveyData() {
-    /*
-    ClientProxy.login(userCredentials).then((result) => {
+  static saveIntakeSurveyData(clientInfo) {
+
+    let {needs, ...clientData} = clientInfo;
+
+    ClientProxy.postClient(clientData).then(() => {
       LHDispatcher.dispatch({
-        type: ActionTypes.LOGIN_REQUEST,
-        username: result.name,
-        userRole: result.roleName,
-        userOrganization: result.organization
+        type: ActionTypes.INTAKE_POSTED
       });
     }).catch((error) => {
       LHDispatcher.dispatch({
         type: ActionTypes.API_REQUEST_FAILED,
-        subType: ActionTypes.LOGIN_REQUEST,
+        subType: ActionTypes.INTAKE_POSTED,
         error: error
       });
     });
-    */
+
+    needs.forEach(need => {
+      ClientProxy.postClientNeed(need).then(() => {
+        LHDispatcher.dispatch({
+          type: ActionTypes.NEED_CREATED
+        });
+      }).catch((error) => {
+        LHDispatcher.dispatch({
+          type: ActionTypes.API_REQUEST_FAILED,
+          subType: ActionTypes.NEED_CREATED,
+          error: error
+        });
+      });
+    });
   }
 
 }

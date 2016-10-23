9458dfc12bb9873fae8d1f7245b2fb8b3378a7dd
Erik Hamilton <ehamilt@gmail.com>
2016-10-22 20:43:40 -0500

    Add a click handler for map markers

--- src/components/map/ShelterMapPage.js
+++ src/components/map/ShelterMapPage.js
@@ -9,13 +9,6 @@ export default class ShelterMapPage extends React.Component {
   static defaultProps = {
     center: {lat: 38.632746, lng: -90.227955},
     zoom: 10,
-    greatPlaces: [
-      {id: '1', lat: 38.59729089, lng: -90.54189364, count: '0'},
-      {id: '2', lat: 38.54256229, lng: -90.28909176, count: '3'},
-      {id: '3', lat: 38.74151895, lng: -90.22576437, count: '7'},
-      {id: '4', lat: 38.60896713, lng: -90.31745003, count: '9'},
-      {id: '5', lat: 38.71566804, lng: -90.32623591, count: '10'},
-    ]
   };
 
   constructor(props) {
@@ -40,6 +33,10 @@ export default class ShelterMapPage extends React.Component {
     this.setState({shelters: ShelterStore.getState().shelters, availableBeds: ShelterStore.getState().availableBeds});
   }
 
+  _onChildClick = (key, childProps) => {
+    alert("This is shelter " + childProps.id);
+  }
+
   shouldComponentUpdate = shouldPureComponentUpdate;
 
   renderChildren() {
@@ -50,7 +47,7 @@ export default class ShelterMapPage extends React.Component {
       const shelter = this.state.shelters[i];
       const count = this.state.availableBeds[i];
       const {id, location, ...other} = shelter;
-      rows.push(<MyGreatPlace key={id} id={id} text={count} lat={location.latitude} lng={location.longitude} {...other} />);
+      rows.push(<MyGreatPlace key={id} id={id} text={count.toString()} lat={location.latitude} lng={location.longitude} {...other} />);
     }
 
     return rows;
@@ -60,7 +57,9 @@ export default class ShelterMapPage extends React.Component {
     return (
       <GoogleMap
         defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}>
+        defaultZoom={this.props.zoom}
+        onChildClick={this._onChildClick}
+      >
         {this.renderChildren()}
       </GoogleMap>
     );
